5890d8f0d9d6ddb93789f9185fbd57e3f010664a
Andrew Starovoyt <astarovoyt@naumen.ru>
2014-01-27 18:15:15 +0600

    fix

--- hibernate-samples/src/main/java/com/kkey/App.java
+++ hibernate-samples/src/main/java/com/kkey/App.java
@@ -2,11 +2,8 @@ package com.kkey;
 
 import java.io.IOException;
 import java.util.Random;
-import java.util.concurrent.Callable;
 
-import org.hibernate.Session;
-
-import com.kkey.entity.EntityObjectSub;
+import com.kkey.entity.EntityDao;
 
 /**
  * Hello world!
@@ -16,61 +13,12 @@ public class App
 {
     public static final Random RND = new Random();
 
-    public static long addEntity()
-    {
-        Session session = HibernateUtils.getCurrentSession();
-        EntityObjectSub object = new EntityObjectSub("Our very first event!");
-        session.save(object);
-
-        return object.getId();
-    }
 
     public static void main(String[] args) throws IOException
     {
-        final long id = HibernateUtils.doCallInTransaction(new Callable<Long>()
-        {
-            public Long call()
-            {
-                return addEntity();
-            }
-        });
+        long entityId = EntityDao.createEntity();
+        EntityDao.updateEntity(entityId);
 
-        HibernateUtils.doInTransaction(new Runnable()
-        {
-            public void run()
-            {
-                System.out.println("start");
-                Session currentSession = HibernateUtils.getCurrentSession();
-                EntityObjectSub loaded = (EntityObjectSub)currentSession.load(EntityObjectSub.class, id);
-
-                updateEntity(loaded);
-
-                new MultiThreadTestCase().startNewThreadUpdate(id);
-
-                System.out.println("end");
-
-            }
-        });
-
-    }
-
-    public static void updateEntity(EntityObjectSub loaded)
-    {
-        Session currentSession = HibernateUtils.getCurrentSession();
-
-        loaded.setSomeValue(String.valueOf(RND.nextInt()));
-        currentSession.saveOrUpdate(loaded);
-        currentSession.flush();
-    }
-
-    public static void updateEntity(final long id)
-    {
-        Session currentSession = HibernateUtils.getCurrentSession();
-        EntityObjectSub loaded = (EntityObjectSub)currentSession.load(EntityObjectSub.class, id);
-
-        loaded.setSomeValue(String.valueOf(RND.nextInt()));
-        currentSession.saveOrUpdate(loaded);
-        currentSession.flush();
     }
 
 }
---
+++ hibernate-samples/src/main/java/com/kkey/entity/EntityDao.java
@@ -0,0 +1,48 @@
+package com.kkey.entity;
+
+import java.util.concurrent.Callable;
+
+import org.hibernate.Session;
+
+import com.kkey.App;
+import com.kkey.HibernateUtils;
+
+/**
+ * @author astarovoyt
+ *
+ */
+public final class EntityDao
+{
+    public static long createEntity()
+    {
+        return HibernateUtils.doCallInTransaction(new Callable<Long>()
+        {
+            public Long call() throws Exception
+            {
+                Session session = HibernateUtils.getCurrentSession();
+                EntityObjectSub object = new EntityObjectSub("Our very first event!");
+                session.save(object);
+
+                return object.getId();
+            }
+        });
+    }
+
+    public static long updateEntity(final long id)
+    {
+        return HibernateUtils.doCallInTransaction(new Callable<Long>()
+        {
+            public Long call() throws Exception
+            {
+                Session currentSession = HibernateUtils.getCurrentSession();
+                EntityObject loaded = (EntityObject)currentSession.load(EntityObjectSub.class, id);
+
+                loaded.setSomeValue(String.valueOf(App.RND.nextInt()));
+                currentSession.saveOrUpdate(loaded);
+                currentSession.flush();
+
+                return loaded.getId();
+            }
+        });
+    }
+}
